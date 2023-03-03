0e2dcfbfe537f3e69f16f4b6dca5c54376df2def
Sakkkkura <yunomewmewmew@gmail.com>
2023-03-03 18:11:13 +0300

    главное окошко и окошко архива и стиль кнопочек

---
+++ diplomnayarrrr/diplomnayarrrr/Views/ArchiveWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace diplomnayarrrr.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ArchiveWindow.xaml
+    /// </summary>
+    public partial class ArchiveWindow : Window
+    {
+        public ArchiveWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
